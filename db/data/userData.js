3d0280a05a649c96920f485e1c06678f638e0609
Lydia <lydiaawood@googlemail.com>
2021-02-18 08:47:02 +0000

    company&user data

--- db/data/userData.js
+++ db/data/userData.js
@@ -1,9 +1,39 @@
 User
 [
   {
-    Name: 'name',
-    Email: 'email address',
-    Password: password',
-    Type: [Job Hunter, Company Admin, Mod]
-  }
+    Name: 'Pauline Prince',
+    Email: 'pauline.prince@charitycrunch.org',
+    Password: 'password',
+    Type: ['Company Admin']
+  },
+  {
+    Name: 'Elizabeth Jayanta',
+    Email: 'elizabeth.jayanta@techcal.co.uk',
+    Password: 'password',
+    Type: ['Company Admin']
+  },
+  {
+    Name: 'Jay Vasudha',
+    Email: 'jv@languagepirates.com',
+    Password: 'password',
+    Type: ['Company Admin']
+  },
+  {
+    Name: 'Katrina Casimiro',
+    Email: 'itskatrina@me.com',
+    Password: 'password',
+    Type: ['Job Hunter']
+  },
+  {
+    Name: 'Zohar Madison',
+    Email: 'z.madison@email.com',
+    Password: 'password',
+    Type: ['Job Hunter']
+  },
+  {
+    Name: 'Soleil Annika',
+    Email: 'soleil.annika@lookmail.com',
+    Password: 'password',
+    Type: ['Job Hunter']
+  },
 ]
