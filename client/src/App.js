70ff54ca4a32d2d05e39740137f7afcf6cdcc9b6
vesnaz <vesna.catic@gmail.com>
2021-02-18 17:08:46 +0000

    plus frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Register from './components/Register'
+import Login from './components/Login'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Companies from './components/Companies'
+import Company from './components/Company'
+//import Jobs from './components/Jobs'
+import Job from './components/Job'
+import PostJob from './components/PostJob'
+
+import 'bulma'
+import './styles/style.scss'
 
 const App = () => (
-  <h1>Hello world</h1>
+  <BrowserRouter>
+    <Navbar />
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/register" component={Register}/>
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/companies" component={Companies}/>
+      <Route exact path="/company" component={Company} />
+      <Route exact path="/job/:jobId" component={Job} />
+      <Route exact path="/company/:companyId/job" component={PostJob} />
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App
