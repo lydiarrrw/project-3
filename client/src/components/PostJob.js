5ea00190db1470740d6159b2c90fb70dd4efed93
Lydia <lydiaawood@googlemail.com>
2021-02-19 11:28:35 +0000

    fixing node issues

--- client/src/components/PostJob.js
+++ client/src/components/PostJob.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
-export default function Home() {
+
+// const inputFields = ['name', ]
+
+export default function PostJob({ handleSubmit}) {
   return <div className="hero">
-    <h1> Welcome Home</h1>
+    <h1> Post a job listing</h1>
+   {/* // form */}
+   <form onSubmit={handleSubmit}>
+  <input 
+  className></input>
+   </form>
+    <p></p>
+    <p>text of job description react component</p>
+    <p>contact details</p>
+    <button>submit</button>
   </div>
 }
