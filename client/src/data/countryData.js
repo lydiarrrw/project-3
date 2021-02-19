5406bb33dd9b649df4e5f1bcd09c253aa72b579d
Lydia <lydiaawood@googlemail.com>
2021-02-19 15:38:15 +0000

    job posting page

---
+++ client/src/data/countryData.js
@@ -0,0 +1,9 @@
+export default [
+  { value: 'none', label: 'Not listed/in job ad' },
+  { value: 'london', label: 'London' },
+  { value: 'manchester', label: 'Manchester' },
+  { value: 'new york', label: 'New York' },
+  { value: 'edinburgh', label: 'Edinburgh' },
+  { value: 'remote', label: 'Remote' },
+  { value: 'tallinn', label: 'Tallinn' }
+]
