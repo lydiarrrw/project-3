05c22e49f6d9e12e2a907471c88a45b38cbc9ebc
Clement Knox <cagtknox@gmail.com>
2021-02-19 10:58:50 +0000

    edits

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,22 +1,7 @@
 import React from 'react'
 
-const pokeImage = 'https://images7.alphacoders.com/592/thumb-1920-592678.jpg'
+
 
 export default function Home() {
-  return <section
-    className="hero is-link is-fullheight-with-navbar"
-  >
-    <div
-      className="hero-body has-text-centered"
-      style={{
-        backgroundImage: `url("${pokeImage}")`,
-        backgroundSize: 'cover'
-      }}>
-      <div className="container has-text-centered">
-        <p className="title has-background-warning is-size-1 has-text-black">
-          {'Gotta\' Catch em\' all'}
-        </p>
-      </div>
-    </div>
-  </section>
+  return <div><h1>HELLO</h1></div>
 }
