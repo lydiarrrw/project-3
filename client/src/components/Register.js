147c1988afc7ea992ce7129bd75b274a9bc2bcd3
vesnaz <vesna.catic@gmail.com>
2021-02-19 17:15:26 +0000

    updated version

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import axios from 'axios'
 
 export default function Register({ history }) {
+  const [error, updateError] = useState('')
 
   const [formData, updateFormData] = useState({
     name: '',
@@ -22,56 +23,59 @@ export default function Register({ history }) {
       console.log(data)
       history.push('/login')
     } catch (err) {
-      console.log(err.response.data)
-      return 'Email address already used.'
+      console.log(err.response.data._message)
+      updateError('Email already exists!')
     }
   }
 
 
-  return <div className="section">
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <div className="field">
-          <label className="label">Name</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              value={formData.name}
-              onChange={handleChange}
-              name={'name'}
-            />
-          </div>
+  return <div className="section bck-img-register">
+    <div className="columns is-centered">
+      <div className="column is-half">
+        <div className="container">
+          <form onSubmit={handleSubmit}>
+            <div className="field">
+              <label className="label">Name</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="text"
+                  value={formData.name}
+                  onChange={handleChange}
+                  name={'name'}
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="text"
+                  value={formData.email}
+                  onChange={handleChange}
+                  name={'email'}
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Password</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  name={'password'}
+                />
+              </div>
+            </div>
+            {/* type */}
+            <button className="button">Submit</button>
+            <p className="content">{ error }</p>
+          </form>
         </div>
-        <div className="field">
-          <label className="label">Email</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              value={formData.email}
-              onChange={handleChange}
-              name={'email'}
-            />
-          </div>
-        </div>
-        <div className="field">
-          <label className="label">Password</label>
-          <div className="control">
-            <input
-              className="input"
-              type="password"
-              value={formData.password}
-              onChange={handleChange}
-              name={'password'}
-            />
-          </div>
-        </div>
-        {/* type */}
-        <button className="button">Submit</button>
-        
-        
-      </form>
+      </div>
     </div>
   </div>
 }
