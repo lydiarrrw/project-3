8963b31938edf0667c5bb015640456acb5444bfc
Tom <tom@briody.net>
2021-02-23 09:52:06 +0000

    tuesday commit

--- client/src/components/Company.js
+++ client/src/components/Company.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-// import { isCreator } from '../lib/auth'
+import { Link, withRouter } from 'react-router-dom'
 
 export default function singleCompany({ match, history }) {
   const id = match.params.companyId
@@ -15,13 +15,15 @@ export default function singleCompany({ match, history }) {
         updateCompany(data)
       } catch (err) {
         console.log(err)
-      }
+      } 
+
     }
     fetchCompany()
   }, [])
 
   if (!company.jobs) return null
   if (!company.comments) return null
+  console.log('COMPANY', company)
 
   function handleComment() {
     axios.post(`/api/company/${id}/comment`, { text }, {
@@ -34,6 +36,7 @@ export default function singleCompany({ match, history }) {
   }
 
   return <div className="companyContainer">
+    
     <h1 className="title is-2 has-text-danger">{company.company}</h1>
     <div className="columns">
       <div className="column is-one-third is-multiline">
@@ -60,8 +63,8 @@ export default function singleCompany({ match, history }) {
           })}
           <h1 className="title mt-6 is-6">Leave a comment below:</h1>
           <div className="control">
-            <input className="input" type="text" placeholder="Type your comment here" onChange={event => setText(event.target.value)} value={text}/>
-            <button onClick={handleComment}className="button is-danger grow mt-4">Submit</button>
+            <input className="input" type="text" placeholder="Type your comment here" onChange={event => setText(event.target.value)} value={text} />
+            <button onClick={handleComment} className="button is-danger grow mt-4">Submit</button>
           </div>
 
         </div>
@@ -77,9 +80,14 @@ export default function singleCompany({ match, history }) {
               <h1><strong>Description:</strong> {job.description}</h1>
               <h1><strong>Salary:</strong> {job.salary}</h1>
               <h1><strong>Location:</strong> {job.location}</h1>
-              <button className="button is-danger grow mt-4">Apply Here!</button>
-            </div>
+              
+              <Link to={{ pathname: `/job/${job._id}`, state: { companyID: id } }}>
+                <button className="button is-success grow mt-4">More Info</button>
+              </Link>
+
+            </div> 
           </div>
+
         })}
 
 
